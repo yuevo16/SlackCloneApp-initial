3ba1e0cbcdd17e0fb3fca47d7fa71c8e2b67d285
yuya <yuevo16@gmail.com>
2020-02-26 15:29:40 +0900

    ログイン機能実装

--- pages/index.vue
+++ pages/index.vue
@@ -2,3 +2,15 @@
   <div class="container">
   </div>
 </template>
+
+<script>
+import { firebase } from '~/plugins/firebase'
+
+export default {
+  mounted() {
+   console.log(this.$store.getters.isAuthenticated)
+  },
+  methods: {
+  }
+}
+</script>
